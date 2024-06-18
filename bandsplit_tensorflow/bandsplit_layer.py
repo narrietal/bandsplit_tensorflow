Fixed error during training given None adaptive shape at the normalization layer.

bandsplit_tensorflow/bandsplit_layer.py
@@ -5,6 +5,7 @@ class BandSplitLayer(tf.keras.layers.Layer):
         super(BandSplitLayer, self).__init__(**kwargs)
         
         nyquist_freq = sr/2 #By default: 8000 Hz
+        self.input_freq_dim = input_freq_dim
         self.freq_resolution = nyquist_freq/(input_freq_dim-1) #By default: 31.25 Hz/bin
 
         # For freq. below 1 kHz -> band size = 100 Hz
@@ -31,7 +32,7 @@ class BandSplitLayer(tf.keras.layers.Layer):
         self.band_dense_layers = []
         self.layer_norm_layers = []
         for i in range(total_num_sub_bands):
-          layer_norm = tf.keras.layers.LayerNormalization(axis=[1, 2]) #Assuming input shape: (batch_samples, time_dim, freq_dim)
+          layer_norm = tf.keras.layers.LayerNormalization(axis=-1) #Assuming input shape: (batch_samples, time_dim, freq_dim)
           self.layer_norm_layers.append(layer_norm)
           dense_layer = tf.keras.layers.Dense(sub_band_feature_dim) # Output shape: (batch_samples, time_dim, sub_band_feature_dim)
           self.band_dense_layers.append(dense_layer)
@@ -46,8 +47,10 @@ class BandSplitLayer(tf.keras.layers.Layer):
               end = int(start + self.suband_size_first_band_bins)
               subband = inputs[:, :, start:end] # shape: (batch_size, time_steps, sub_band_width)
               
+              subband = tf.keras.layers.Reshape((-1, self.suband_size_first_band_bins))(subband) # Reshape the input tensor to combine time_dim and freq_dim (required because time_dim during training is None and layernorm breaks)
               norm_layer = self.layer_norm_layers[i]
               norm_subband = norm_layer(subband) # shape: (batch_size, time_steps, sub_band_width)
+              norm_subband = tf.keras.layers.Reshape((-1, self.suband_size_first_band_bins))(norm_subband) # Reshape back to the original dimensions (required because time_dim during training is None and layernorm breaks)
               
               dense_layer = self.band_dense_layers[i]
               embedded_subband = dense_layer(norm_subband) # shape: (batch_size, time_steps, sub_band_feature_dim)
@@ -59,8 +62,10 @@ class BandSplitLayer(tf.keras.layers.Layer):
               end = int(start + self.suband_size_second_band_bins)
               subband = inputs[:, :, start:end] # shape: (batch_size, time_steps, sub_band_width)
               
+              subband = tf.keras.layers.Reshape((-1, self.suband_size_second_band_bins))(subband) # Reshape the input tensor to combine time_dim and freq_dim (required because time_dim during training is None and layernorm breaks)
               norm_layer = self.layer_norm_layers[i+self.num_sub_bands_first_band]
               norm_subband = norm_layer(subband) # shape: (batch_size, time_steps, sub_band_width)
+              norm_subband = tf.keras.layers.Reshape((-1, self.suband_size_second_band_bins))(norm_subband) # Reshape back to the original dimensions (required because time_dim during training is None and layernorm breaks)
               
               dense_layer = self.band_dense_layers[i+self.num_sub_bands_first_band]
               embedded_subband = dense_layer(norm_subband) # shape: (batch_size, time_steps, sub_band_feature_dim)
@@ -72,8 +77,10 @@ class BandSplitLayer(tf.keras.layers.Layer):
               end = int(start + self.suband_size_third_band_bins)
               subband = inputs[:, :, start:end] # shape: (batch_size, time_steps, sub_band_width)
               
+              subband = tf.keras.layers.Reshape((-1, self.suband_size_third_band_bins))(subband) # Reshape the input tensor to combine time_dim and freq_dim (required because time_dim during training is None and layernorm breaks)
               norm_layer = self.layer_norm_layers[i+self.num_sub_bands_first_band+self.num_sub_bands_second_band]
               norm_subband = norm_layer(subband) # shape: (batch_size, time_steps, sub_band_width)
+              norm_subband = tf.keras.layers.Reshape((-1, self.suband_size_third_band_bins))(norm_subband) # Reshape back to the original dimensions (required because time_dim during training is None and layernorm breaks)
               
               dense_layer = self.band_dense_layers[i+self.num_sub_bands_first_band+self.num_sub_bands_second_band]
               embedded_subband = dense_layer(norm_subband) # shape: (batch_size, time_steps, sub_band_feature_dim)