df27d85171e7c4952a86ddb27cb89018c9a6c175
nial <nial@bang-olufsen.dk>
2024-06-19 13:22:01 +0000

    Updated package version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     
 setup(
     name='bandsplit_tensorflow',
-    version='0.1.1',
+    version='0.1.2',
     description='A non-official implementation of the BandSplit technique as a TensorFlow layer.',
     long_description=long_description,
     long_description_content_type="text/markdown",
