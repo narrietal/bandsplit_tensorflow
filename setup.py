f2aee6101d3599e36759b95d9ce794bee94205b4
nial <nial@bang-olufsen.dk>
2024-06-19 13:20:48 +0000

    Updated setup.py file with description.

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+    
 setup(
     name='bandsplit_tensorflow',
     version='0.1.1',
     description='A non-official implementation of the BandSplit technique as a TensorFlow layer.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/narrietal/bandsplit_tensorflow",
     author='Nicolas Arrieta Larraza',
     author_email='n.arrieta.larraza@gmail.com',
     packages=find_packages(),
